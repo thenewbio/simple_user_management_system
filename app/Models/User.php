6e0fc102509408db169e7f7f7fbaad4b87deb201
innocent <okamainnocent2020@gmail.com>
2022-07-18 22:08:07 +0100

    changed user.model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends  Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
     protected $fillable = [
         'Name', 'Email', 'Phone'
     ];
