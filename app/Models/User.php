12830315edf4601556a16104fa2788dbe67eada6
innocent <okamainnocent2020@gmail.com>
2022-07-18 23:47:22 +0100

    userController

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends  Authenticatable
+class User extends Model
 {
-    use HasFactory, Notifiable;
-
+    use HasFactory;
     protected $fillable = [
         'Name', 'Email', 'Phone'
     ];
