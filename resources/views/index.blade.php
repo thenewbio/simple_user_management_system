35e0f2a6917f355c26690b589588d4ef5d830d7d
innocent <okamainnocent2020@gmail.com>
2022-07-19 10:10:03 +0100

    changed  userController

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,7 +45,7 @@
                                             <a href="{{ url($user->id . '/edit') }}" title="Edit Student"><button class="ml-4 bg-blue-500 tracking-wide text-white px-6 py-2 inline-block mb-6 shadow-lg rounded hover:shadow"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
                                             <form method="POST" action="{{ url('/' . '/' . $user->id) }}" accept-charset="UTF-8" style="display:inline">
                                             {{ method_field('DELETE') }}
-                                                {{ csrf_field() }}
+                                                @csrf
                                                 <button type = "submit" class="ml-4 bg-red-500 tracking-wide text-white px-6 py-2 inline-block mb-6 shadow-lg rounded hover:shadow" title="Delete Student" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
                                             </form>
                                         </td>
