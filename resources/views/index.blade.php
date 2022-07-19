594942fb4967839cc6e04315e148fdeca5427b26
innocent <okamainnocent2020@gmail.com>
2022-07-19 12:22:28 +0100

    changed  userController

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,7 +46,7 @@
                                         
                                             <a href="{{ url($user->id . '/edit') }}" title="Edit Student"><button class="ml-4 bg-blue-500 tracking-wide text-white px-6 py-2 inline-block mb-6 shadow-lg rounded hover:shadow"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
                                           
-                                                <button class="ml-4 bg-red-500 tracking-wide text-white px-6 py-2 inline-block mb-6 shadow-lg rounded hover:shadow" title="Delete Student" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                                            <a href="{{ url($user->id . '/') }}"<button class="ml-4 bg-red-500 tracking-wide text-white px-6 py-2 inline-block mb-6 shadow-lg rounded hover:shadow" title="Delete Student" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button></a>
                                           
                                         </td>
                                     </tr>
